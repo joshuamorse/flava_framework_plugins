82c01256f779e4dde8c301fb650267557faed9d8
Joshua Morse <dashvibe@gmail.com>
2010-04-11 19:57:43 -0500

    starting writing use_functions; need to load in plugin config here

---
+++ functions/lib/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function use_functions($definition)
+{
+  global $_autoloaded_plugin_info;
+  //print_r($_autoloaded_plugin_info); die('d');
+  //print_r($_autoloaded_plugin_info); die();
+  //require($_plugin['dir']['user'].$definition.'.php');
+}
